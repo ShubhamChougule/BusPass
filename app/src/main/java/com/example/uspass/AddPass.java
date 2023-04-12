8c0f547ee290c0bfd4dc8cd04554114e98c9e89d
Shubham Chougule <shubham7102001@gmail.com>
2023-04-12 16:37:27 +0530

    last update

--- app/src/main/java/com/example/uspass/AddPass.java
+++ app/src/main/java/com/example/uspass/AddPass.java
@@ -3,6 +3,8 @@ package com.example.uspass;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class AddPass extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class AddPass extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_pass);
+
+
     }
 }
